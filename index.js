5b2ce5c5b123aaf86f865cd1728395a922ec750c
pranaysankineni <pranayrao475@gmail.com>
2021-08-20 19:13:51 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Code your solutions in this file
+function writeCards(names){
+    let message = [];
+   
+    for(let i = 0; i < names.length; i++){
+        message.push(`Thank you, ${names[i]}, for the wonderful surprise gift!`);
+    }
+    return message;
+}
+
+function countDown(num){
+    while(num >= 0){
+        console.log(num)
+        num --;
+    }
+} 
